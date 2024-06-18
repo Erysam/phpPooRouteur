bf8d655f81e9c4e4ec7239808b21238022f2e37e
Erys <1maryseal@gmail.com>
2024-06-18 18:42:02 +0200

    ajout des use et modifs

--- public/indexRouteur.php
+++ public/indexRouteur.php
@@ -1,17 +1,16 @@
 <?php
 
+use Router\Router;
+
+
 require './../vendor/autoload.php';
 
 //use \Colors\RandomColor;
 
-use Router\Router;
-
-
-
-$routeur = new Router;
+$router = new Router();
 
 //ci-dessous des roots stockés dans un tab, attribut  de ma classe routeur
-$router->register('/', function () { // on lui donne le registre racine '/' et on lui passe un function anonyme
+$router->register('/', function () { // on lui donne le registre racine '/' et on lui passe un function anonyme (clé et action, pour le tab de la classe)
     return 'HomePage';
 });
 
@@ -19,4 +18,9 @@ $router->register('/contact', function () {
     return 'Contact Page ';
 });
 
+
+echo '<pre>';
+var_dump($router);
+echo '</pre>';
 // on doit également faire une methode resolve ou run pour resoudre tout les pb de root
+//$router->resolve();
