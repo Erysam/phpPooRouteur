7b65482e90012966e6017f403fdfeedf34ebe4d0
Erys <1maryseal@gmail.com>
2024-06-18 13:07:38 +0200

    add

---
+++ public/indexRouteur.php
@@ -0,0 +1,16 @@
+<?php
+
+//require './../vendor/autoload.php';
+
+$routeur = new Router;
+
+//ci-dessous des roots stockés dans un tab, attribut  de ma classe routeur
+$router->register('/', function () { // on lui donne le registre racine '/' et on lui passe un function anonyme
+    return 'HomePage';
+});
+
+$router->register('/contact', function () {
+    return 'Contact Page ';
+});
+
+// on doit également faire une methode resolve ou run pour resoudre tout les pb de root
