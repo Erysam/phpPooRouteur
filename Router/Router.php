22a32fa5b8b0ba46f8341062924aee6db6dce9bc
Erys <1maryseal@gmail.com>
2024-06-18 18:43:01 +0200

    ecriture de la methode de la classe Router

--- Router/Router.php
+++ Router/Router.php
@@ -4,4 +4,13 @@ namespace Router;
 
 class Router
 {
+    //on declare un attribut tab 
+    private array $routes;
+
+    // callable (call : qui peut etre appelé) type de donnée qui represente une fonction, 
+    //souvent used pour passer une focntion en parametre à une methode (utilisé sur les callback, les tris personnalisés)
+    public function register(string $path, callable $action): void
+    {
+        $this->routes[$path] = $action;
+    }
 }
