f9245c779c01d43d04ddf1fe7b6391de67aca496
Erys <1maryseal@gmail.com>
2024-06-25 16:23:31 +0200

    suite du code

--- Router/Router.php
+++ Router/Router.php
@@ -2,6 +2,8 @@
 
 namespace Router;
 
+use Exceptions\RouteNotFoundException;
+
 class Router
 {
     //on declare un attribut tab 
@@ -9,14 +11,19 @@ class Router
 
     // callable (call : qui peut etre appelé) type de donnée qui represente une fonction, 
     //souvent used pour passer une focntion en parametre à une methode (utilisé sur les callback, les tris personnalisés)
-    public function register(string $path, callable $action): void
+    public function register(string $path, callable|array $action): void // c ets une union de typage '|' sinon peut mettre un mixed (pas réduit les avantages du typage)
     {
         $this->routes[$path] = $action;
     }
 
-    public function resolve(string $uri)
+    public function resolve(string $uri): mixed //(car on peut revoyer un callable ou une exception)
     {
         $path = explode('?', $uri)[0]; //la 1er clé sera tjrs la bonne
-        $action = $this->routes[$path] ?? null; //??null ets un ternaire : $message = ($age >= 18) ? 'Vous êtes majeur.' : 'Vous êtes mineur.';
+        $action = $this->routes[$path] ?? null; //?? null ets un ternaire: si $this->routes[$path] est defini et différent de null on le met dans $action
+        //exemple de ternaire plus parlant: $message = ($age >= 18) ? 'Vous êtes majeur.' : 'Vous êtes mineur.';
+        if (!is_callable($action)) { //si c'est différent de is callable qui est methode a appliquer si le path ets difini
+            throw new RouteNotFoundException();
+        }
+        return $action(); // $action est un callable donc on met des parentheses
     }
 }
